0c5c0c962342e5336f8db5b1cb8b960dcf9a62cb
Pieterjan Criel <Pieterjan@ballooninc.be>
2017-12-07 15:54:00 +0100

    clean routes

--- routes/index.js
+++ routes/index.js
@@ -22,23 +22,4 @@ routes.get('/salary/:country', function(req, res) {
 	});
 });
 
-routes.get('/age/:country', function(req, res) {
-	var country = req.params['country'];
-	var result = {};
-	MongoClient.connect(url, function(err, db) {
-	  if (err) throw err;
-	  var query = { _id: { $in : [country]} };
-	  db.collection("stackoverflowsurvey")
-		.aggregate(
-			[{$match: {Country: {'$regex' : '^'+country+'$', '$options' : 'i'} }},
-			{$group: {_id:"$Country", average: {$avg:"$Age"} } }])
-		.toArray(function(err, result) {
-	    if (err) throw err;
-	    db.close();
-	    res.status(200).json(result);
-	  });
-	});
-});
-
-
 module.exports = routes;
