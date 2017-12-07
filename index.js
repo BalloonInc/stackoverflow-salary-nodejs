60334bfee33c3c7f581aef62243235543d9dec9b
Pieterjan Criel <Pieterjan@ballooninc.be>
2017-12-07 15:50:05 +0100

    fix readme

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const app = require('express')();
 const routes = require('./routes');
 
-
 app.use('/api', routes);
 
-
-// Setup the Node.JS server
 var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
-   
+
    console.log("listening at http://%s:%s", host, port)
 })
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var routes = require('express').Router();
 var MongoClient = require('mongodb').MongoClient;
 
+// use the shell script init_db.sh to insert the data in a mongodb
 var url = "mongodb://localhost:27017/stackoverflowsurveydatabase";
 
 routes.get('/salary/:country', function(req, res) {
@@ -9,12 +10,35 @@ routes.get('/salary/:country', function(req, res) {
 	MongoClient.connect(url, function(err, db) {
 	  if (err) throw err;
 	  var query = { _id: { $in : [country]} };
-	  db.collection("stackoverflowsurvey").aggregate([{$match: {Country: {'$regex' : '^'+country+'$', '$options' : 'i'} }},{$group: {_id:"$Country", average: {$avg:"$Salary"} } }]).toArray(function(err, result) {
+	  db.collection("stackoverflowsurvey")
+		.aggregate(
+			[{$match: {Country: {'$regex' : '^'+country+'$', '$options' : 'i'} }},
+			{$group: {_id:"$Country", average: {$avg:"$Salary"} } }])
+		.toArray(function(err, result) {
 	    if (err) throw err;
 	    db.close();
 	    res.status(200).json(result);
 	  });
 	});
 });
+
+routes.get('/age/:country', function(req, res) {
+	var country = req.params['country'];
+	var result = {};
+	MongoClient.connect(url, function(err, db) {
+	  if (err) throw err;
+	  var query = { _id: { $in : [country]} };
+	  db.collection("stackoverflowsurvey")
+		.aggregate(
+			[{$match: {Country: {'$regex' : '^'+country+'$', '$options' : 'i'} }},
+			{$group: {_id:"$Country", average: {$avg:"$Age"} } }])
+		.toArray(function(err, result) {
+	    if (err) throw err;
+	    db.close();
+	    res.status(200).json(result);
+	  });
+	});
+});
+
 
 module.exports = routes;
