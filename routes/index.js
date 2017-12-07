6935ffe10612f1aa828d4ab23ddf67d12c048180
Pieterjan Criel <crielpieterjan@gmail.com>
2017-12-07 15:04:47 +0100

    better routes

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var MongoClient = require('mongodb').MongoClient;
 
 var url = "mongodb://localhost:27017/stackoverflowsurveydatabase";
 
-
-routes.get('/', function(req, res) {
+routes.get('/salary/:country', function(req, res) {
+	var country = req.params['country'];
 	var result = {};
 	MongoClient.connect(url, function(err, db) {
 	  if (err) throw err;
-	  var query = { Country: "Belgium" };
-	  db.collection("stackoverflowsurvey").find(query).toArray(function(err, result) {
+	  var query = { _id: { $in : [country]} };
+	  db.collection("stackoverflowsurvey").aggregate([{$match: {Country: {'$regex' : '^'+country+'$', '$options' : 'i'} }},{$group: {_id:"$Country", average: {$avg:"$Salary"} } }]).toArray(function(err, result) {
 	    if (err) throw err;
 	    db.close();
 	    res.status(200).json(result);
